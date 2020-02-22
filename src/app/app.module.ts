89432659118adad9ea26041ea3fc76197470155f
Panupong <5635512110@psu.ac.th>
2020-02-22 08:27:47 +0700

    update ng-rx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositoriesInjector } from '../RepositoriesInjector';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RepositoriesInjector } from '../../RepositoriesInjector';
+import { RepositoriesInjector } from '../../../RepositoriesInjector';
 import { CatsRepository } from 'src/RepositoriesInjector/Repositories/CatsRepository';
 
 @Component({
